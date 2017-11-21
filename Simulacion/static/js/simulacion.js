31dd0d9c608252f74ce0f58b8e85b79b936f04a3
KatlheenEscoto <escotokatlheen@gmail.com>
2017-11-20 22:13:05 -0600

    Revert "Merge branch 'master' of https://github.com/KatlheenEscoto/ARC115"
    This reverts commit c36bf390abec569a442874c18b28482e455b4293, reversing
    changes made to 69ee700a46f6ff8d87dfd916c1722fa2a6fac98d.

--- Simulacion/static/js/simulacion.js
+++ Simulacion/static/js/simulacion.js
@@ -400,7 +400,7 @@ function suspender(id_fila){
         pagsM = parseInt($('#'+id_fila).find('td').eq(6).text());
         $('#'+id_fila).find('td').eq(5).text(pagsM);
         $('#'+id_fila).find('td').eq(6).text(pagsP);
-        actualizarEstadisticos(pags,-pagsM,-pagsP);  // <---- Que le pongo a esta vaina :
+        actualizarEstadisticos(0,pags,-pagsP,-pagsM);
         $('#tabProcesElim').prop('disabled',true);
         $('#tabProcesListo').prop('disabled',true);
         $('#tabProcesSus').prop('disabled',true);
@@ -535,7 +535,7 @@ function fifo(cantMemPDis,cantMemSDis,tamPag,paginas,nombrePro,idPro){
 	var paginaCargada=0;
 	var cargadaCambMS=0;
 
-for (var k = 0; k <= paginas; k++) {
+for (var k = 0; k <= paginas; k++){
 	paginaCargada=0;
 	cargadaCambMS=0;
 	menorid = 3000;
@@ -574,6 +574,7 @@ for (var k = 0; k <= paginas; k++) {
 				$(this).find('td').eq(2).text(nombrePro);
 				$(this).find('td').eq(3).text(k);
 				actualizarEstadisticos(1,0,1);
+
 				cargadasMS++;
 				paginaCargada=1;
 				}	
@@ -603,6 +604,7 @@ for (var k = 0; k <= paginas; k++) {
 							$(this).find('td').eq(2).text(nombCamb);
 							$(this).find('td').eq(3).text(numPagCamb);
 							actualizarEstadisticos(1,0,1);
+							actualizarProcesoFIFO(idCamb);
 							cargadaCambMS=1;
 						}	
 					});
@@ -640,12 +642,33 @@ function actualizarProcesoFIFO(idPro){
 	var idActual=0;
 	var pagMP=0;
 	var pagMS=0;
+	var hayPagsMP=false;
+	var band = 0;
 	var estado="Espera";
+
 	$('#tabProcesBody tr').each(function() {
 		idActual=parseInt($(this).find('td').eq(0).text());
 		if (idPro==idActual) {
 			pagMP=parseInt($(this).find('td').eq(5).text());
 			pagMS=parseInt($(this).find('td').eq(6).text());
-		};
+
+			$('#tabMemPri tbody tr').each(function() {
+				band=parseInt($(this).find('td').eq(2).text());
+			    if( band == idPro){
+			    	hayPagsMP =true;
+		
+			    }
+			});
+
+			if (hayPagsMP) {
+				$(this).find('td').eq(5).text(pagMP-1);
+				$(this).find('td').eq(6).text(pagMS+1);							
+			}else {
+				$(this).find('td').eq(4).text("Espera");
+				$(this).find('td').eq(5).text(pagMP-1);
+				$(this).find('td').eq(6).text(pagMS+1);				
+			}
+
+		}
 	});
 }
